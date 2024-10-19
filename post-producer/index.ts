7a1519fff676718c12cbb5533f208c16868f96cb
Sathwik <hsathwik002@gmail.com>
2024-10-19 18:27:37 +0530

    Feath:server

--- post-producer/index.ts
+++ post-producer/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
